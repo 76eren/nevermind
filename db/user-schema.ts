3a46d31888cc25e6562704dbaece47b739603cb7
Eren <e.basaran2004@gmail.com>
2026-09-17 20:55:36 +0200

    feat: add migration to user to also have a banner

--- db/user-schema.ts
+++ db/user-schema.ts
@@ -5,7 +5,8 @@ export const user = pgTable("user", {
   name: text("name").notNull(),
   email: text("email").notNull().unique(),
   emailVerified: boolean("email_verified").default(false).notNull(),
-  image: text("image"),
+  image: text("image"), // Profile picture
+  banner: text("banner"), // Banner picture
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at")
     .defaultNow()
